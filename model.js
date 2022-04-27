25ac99aa58fdb35ecf01a47bef5c62fad971c06a
BuildTools <unconfigured@null.spigotmc.org>
2022-04-28 00:40:59 +0200

    ouai2

--- model.js
+++ model.js
@@ -4,142 +4,88 @@
 const Sqlite = require('better-sqlite3');
 const cross_fetch = require("cross-fetch");
 const e = require("express");
+const {response} = require("express");
 
 let db = new Sqlite('db.sqlite');
 
 let apiKey = "1e976b5a411fe60b504a811adb4817ee";
 
 
-/* Lire le contenu d'une recette à partir de son identifiant.
-
-Cette fonction prend en argument un identifiant de recette.
-
-Elle renvoie une recette sous la forme d'un objet :
-- title: son titre
-- description: la description textuelle de la recette
-- duration: la durée totale de la recette
-- img: l'url de son image
-- ingredients: une liste d'ingrédients (pour chacun il y a un champ name)
-- stages: une liste d'étapes pour la recette (chacune contient un champ description)
-
-Cette fonction renvoie null si l'identifiant n'existe pas.
- */
-exports.read = (id) => {
-    let found = db.prepare('SELECT * FROM recipe WHERE id = ?').get(id);
-    if (found !== undefined) {
-        found.ingredients = db.prepare('SELECT name FROM ingredient WHERE recipe = ? ORDER BY rank').all(id);
-        found.stages = db.prepare('SELECT description FROM stage WHERE recipe = ? ORDER BY rank').all(id);
-        return found;
-    } else {
-        return null;
-    }
-};
-
-/* Fonction pour créer une nouvelle recette dans la base.
-
-Prend en paramètre un objet javascript décrivant la recette sous la forme suivante :
-recipe = {
-  title: 'text',
-  description: 'text',
-  duration: 'text',
-  ingredients: [
-    {name: 'text'},
-    ...
-  ],
-  stages: [
-    {description: 'text'},
-    ...
-  ]
+exports.login = function (user,password){
+    let result = db.prepare('SELECT id FROM user WHERE name = ? AND password = ?').get(user,password);
+    if (result === undefined) return -1;
+    return result.id
 }
 
-Cette fonction retourne l'identifiant de la recette créée.
-*/
-exports.create = function(recipe) {
-    let id = db.prepare('INSERT INTO recipe (title, img, description, duration) VALUES (@title, @img, @description, @duration)').run(recipe).lastInsertRowid;
-
-    let insert1 = db.prepare('INSERT INTO ingredient VALUES (@recipe, @rank, @name)');
-    let insert2 = db.prepare('INSERT INTO stage VALUES (@recipe, @rank, @description)');
-
-    let transaction = db.transaction((recipe) => {
-        for (let j = 0; j < recipe.ingredients.length; j++) {
-            insert1.run({recipe: id, rank: j, name: recipe.ingredients[j].name});
-        }
-        for (let j = 0; j < recipe.stages.length; j++) {
-            insert2.run({recipe: id, rank: j, description: recipe.stages[j].description});
-        }
-    });
-
-    transaction(recipe);
-    return id;
+exports.sign_up = function (user, password){
+    let result = db.prepare('INSERT INTO user (name,password) VALUES (?,?)').run(user,password);
+    return result.lastInserRowId;
 }
 
-/* Fonction pour mettre à jour une recette de la base.
-
-Un identifiant de recette doit être passé en premier argument.
-Le second argument est un objet javascript au même format que pour la fonction create.
-
-Cette fonction revoie true si l'identifiant existe dans la base.
-*/
-exports.update = function(id, recipe) {
-    let result = db.prepare('UPDATE recipe SET title = @title, img = @img, description = @description WHERE id = ?').run(recipe, id);
-    if (result.changes == 1) {
-        let insert1 = db.prepare('INSERT INTO ingredient VALUES (@recipe, @rank, @name)');
-        let insert2 = db.prepare('INSERT INTO stage VALUES (@recipe, @rank, @description)');
-
-        let transaction = db.transaction((recipe) => {
-            db.prepare('DELETE FROM ingredient WHERE recipe = ?').run(id);
-            for (let j = 0; j < recipe.ingredients.length; j++) {
-                insert1.run({recipe: id, rank: j, name: recipe.ingredients[j].name});
-            }
-            db.prepare('DELETE FROM stage WHERE recipe = ?').run(id);
-            for (let j = 0; j < recipe.stages.length; j++) {
-                insert2.run({recipe: id, rank: j, description: recipe.stages[j].description});
-            }
-        });
-
-        transaction(recipe);
-        return true;
-    }
-    return false;
+exports.getDetailAccount = function (id){
+    return db.prepare('SELECT name FROM user WHERE id = ?').get(id);
 }
 
-/* Fonction pour effacer une recette dans la base à partir de son identifiant */
-exports.delete = function(id) {
-    db.prepare('DELETE FROM recipe WHERE id = ?').run(id);
-    db.prepare('DELETE FROM ingredient WHERE recipe = ?').run(id);
-    db.prepare('DELETE FROM stage WHERE recipe = ?').run(id);
-}
-
-/* Recherche d'une recette par requête, avec pagination des résultats
-
-Cette fonction prend en argument la requête sous forme d'une chaîne de caractères
-et le numéro de la page de résultats.
-
-Cette fonction retourne un dictionnaire contenant les champs suivants :
-- results: liste de recettes (version courte contenant l'identifiant de la recette, son titre et l'url de son image)
-- num_found: le nombre de recettes trouvées
-- query: la requête
-- next_page: numero de la page suivante
-- page: numero de la page courante
-- num_pages: nombre total de pages
-*/
-
-
-async function search(query){
-    let resul = {result : [] ,nbrpage : null };
-    let url="https://api.themoviedb.org/3/search/movie?api_key="+apiKey+"&language=en-US&query="+query+"&page=1&include_adult=false"
+async function search(query,page){
+    let resul = {result : [] ,nbrpage : null ,actuPage : null, nextPage : null,query : null , id : null};
+    let url="https://api.themoviedb.org/3/search/movie?api_key="+apiKey+"&language=en-US&query="+query+"&page="+ page +"&include_adult=false"
     return cross_fetch.fetch(url).then(function (response) {
         return response.json()
     }).then(function (text) {
         resul.result=text['results'];
-        resul.nbrpage=text['page'];
+        resul.actuPage=text['page'];
+        resul.nbrPage=text['total_pages'];
+        resul.nextPage=resul.actuPage+1;
+        resul.query = query;
         return resul;
     })
 
 }
 
-exports.search =search;
+async function searchMovie(id){
+    let url = "https://api.themoviedb.org/3/movie/"+id +"?api_key="+apiKey+"&language=en-US";
+    let urlProvider = "https://api.themoviedb.org/3/movie/"+ id +"/watch/providers?api_key=" + apiKey;
+    return cross_fetch.fetch(url).then(function (response) {
+        return response.json()
+    }).then(function (text) {
+            let provider = fetchProvider(id);
+            let time = text['runtime'];
+            let h = Math.trunc(parseInt(time)/60);
+            let min = parseInt(time) % 60;
+                return {
+                    img: text['poster_path'],
+                    titre: text['original_title'],
+                    resume: text['overview'],
+                    rate: text['popularity'],
+                    genre: text['genres'],
+                    release: text['release_date'],
+                    time: h + "h" + min + "m",
+                    available: provider
+                };
+        })
+}
 
+async function fetchProvider(id){
+    let urlProvider = "https://api.themoviedb.org/3/movie/"+ id +"/watch/providers?api_key=" + apiKey;
+    return cross_fetch.fetch(urlProvider).then(function (response) {
+        if (response.status >= 200 && response.status <= 299) {
+            return response.json();
+        } else {
+            throw Error(response.statusText);
+        }
+    }).then(function (text2){
+        return {available: text2['results']['US']['flatrate']
+        };
+    }).catch((error) => {
+        console.log(error);
+    });
+
+
+}
+
+
+exports.searchMovie = searchMovie;
+exports.search =search;
 
  function fetchNexPage(query){
 
--- server.js
+++ server.js
@@ -9,27 +9,89 @@ let app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const cookieSession = require('cookie-session');
-const {getters} = require("better-sqlite3/lib/methods/wrappers");
-app.use(cookieSession({
-    secret: 'mot-de-passe-du-cookie',
-}));
-
 app.engine('html', mustache());
 app.set('view engine', 'html');
 app.set('views', './views');
+app.use('/static', express.static('public'));
+
+const cookieSession = require('cookie-session');
+
+app.use(cookieSession({
+    secret: 'projetMovie',
+}));
+
+function is_authenticated(req,res,next){
+    if (req.session.user !== undefined){
+        return next();
+    }
+    res.status(401).send('Authentication required');
+}
+
+app.use(function (req,res,next){
+    if (req.session.user !==undefined){
+        res.locals.authenticated = true;
+        res.locals.name = req.session.name;
+    }
+    return next();
+});
+
 
 app.get('/', (req, res) => {
     res.render('header');
 });
 
 
-app.get('/search',async (req, res) => {
-    let found = await model.search(req.query.query);
-    console.log(found)
+app.post('/login' , (req,res) =>{
+    let user =model.login(req.body.user,req.body.password);
+    if (user !==-1){
+        req.session.user= user;
+        req.session.name=req.body.user;
+        res.redirect('/')
+    }else {
+        res.redirect('/login');
+    }
+});
 
+app.get('/login',(req,res)=>{
+   res.render('login')
+});
+
+app.post('/sign_up' , (req,res) =>{
+    let user = model.sign_up(req.body.user,req.body.password);
+    if (user !==-1){
+        req.session.user= user;
+        req.session.name=req.body.user;
+        res.redirect('/')
+    }else {
+        res.redirect('/');
+    }
+});
+
+app.get('/sign_up',(req,res)=>{
+    res.render('sign_up')
+});
+
+app.get('/logout',(req,res)=>{
+   req.session = null;
+   res.redirect('/')
+});
+
+app.get('/account',is_authenticated,(req,res)=>{
+    let found = model.getDetailAccount(req.session.user);
+    res.render('account',found);
+});
+
+
+
+app.get('/:page/search',async (req, res) => {
+    let found = await model.search(req.query.query,req.params.page);
     res.render('search',found);
+});
 
+app.get('/movie/:id',async (req, res) => {
+    let found = await model.searchMovie(req.params.id);
+    console.log(found);
+    res.render('movie',found);
 });
 
 app.listen(3000, () => console.log('listening on http://localhost:3000'));
